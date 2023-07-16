35f4522a9955fa2d383cbb94bb6fa3c5fdef798d
YordanKarpachev <yordkarp@icloud.com>
2023-07-16 14:12:50 +0200

    Project setup

---
+++ EventsDemo/src/test/java/com/example/EventsDemo/EventsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
